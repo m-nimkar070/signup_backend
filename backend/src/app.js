3811b6f53072c20cbbb3616b2789973111f273ad
Mahesh M Nimkar <m.nimkar070@gmail.com>
2024-06-13 16:50:33 +0530

    changes in app fi

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,20 +8,15 @@ const { jwtStrategy } = require("./config/passport");
 const passport = require("passport");
 
 const app = express();
-
+//Cors Policy
+app.use(cors());
 // parse json request body
 app.use(express.json());
 
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("*", cors());
 
 app.use(passport.initialize());
 passport.use("jwt", jwtStrategy);
